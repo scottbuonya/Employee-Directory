77e4c6dae30e14e00ad18bd5ee6c32af81a3f405
Scott Buonya <scottbuonya@gmail.com>
2020-09-28 20:42:56 -0400

    added a random user api into a json file

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,8 +1,40 @@
-import React from "react";
-import HelloBootstrap from "./components/HelloBootstrap";
+import React from 'react';
+import './App.css';
+import results from './names.json';
 
-function App() {
-  return <HelloBootstrap />;
+class App extends React.Component  {
+ state={
+   nameList: results
+ }
+  render() { 
+    return (
+      <>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">first</th>
+              <th scope="col">last</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.nameList.map(name=>(
+            <tr>
+
+
+            <th scope="row">{results}</th>
+              <td>{results.name}</td>
+              <td>{results.location}</td>
+              <td>{results.picture}</td>
+            </tr>
+
+            ))}
+          </tbody>
+        </table>
+      </>
+  );
+ }
+
 }
 
 export default App;
